b41906ff0e55cde5ec95d5a2a69376c74f99ca93
KAYSILVHA <kaysilvha@gmail.com>
2023-08-09 01:31:15 -0300

    ajustes e menu sanduiche

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(){
+  const btnMobilie = document.getElementById('nav');
+  nav.classList.toggle('active');
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+
+
+
